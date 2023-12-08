9824c8816668b16ce0535a9676375caa4a6e7c1a
Flo <floriandufaure@gmail.com>
2023-12-08 04:10:14 +0100

    Modèle a vraiment pas ouf marche

--- server.py
+++ server.py
@@ -1,29 +1,41 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+import keras
+import pickle
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+#import translate
+from translate import Translator
 
 # Load the tensorflow model
 from tensorflow.keras.models import load_model
-model = load_model('notre_model.keras')
+# model = load_model('notre_model.keras')
+model = keras.models.load_model('true_model.h5')
+# loading
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
-@app.route('/api/emotions/')
+app = Flask(__name__)
+@app.route('/api/emotions/', methods=['POST'])
 def emotions():
-    # get the text from the request
-    text = request.args.get('text')
-    # make the prediction
-    prediction = model.predict(text)
+    data = request.get_json()
+    if 'text' in data:
+        text = data['text']
+    else:
+        return "ERROR: no text provided"
+    
+    #translate the text to english
+    translator = Translator(to_lang="english", from_lang="french")
+    print("TRANSLATE : ", translator.translate(text))
 
     emotions = ['admiration', 'amusement', 'anger', 'annoyance', 'approval', 'caring', 'confusion', 'curiosity', 'desire', 'disappointment', 'disapproval', 'disgust', 'embarrassment', 'excitement', 'fear', 'gratitude', 'grief', 'joy', 'love', 'nervousness', 'optimism', 'pride', 'realization', 'relief', 'remorse', 'sadness', 'surprise', 'neutral']
-    emotions_dict = {}
-
-
-    for i in range(len(prediction)):
-        emotions_dict[emotions[i]] = prediction[i]
+    dict = {}
+    text_seq = tokenizer.texts_to_sequences(text)
+    text_padded = pad_sequences(text_seq, maxlen=200, padding='post', truncating='post')
+    for i in range(len(emotions)):
+        dict[emotions[i]] = model.predict(text_padded)[0][i]
 
     # 3 best emotions
-    top_3 = sorted(emotions_dict.items(), key=lambda x: x[1], reverse=True)[:3]
+    top_3 = sorted(dict.items(), key=lambda x: x[1], reverse=True)[:3]
 
-    # return the prediction
-    response = jsonify({
-        'emotions': top_3
-    })
-
-    return response
+    #reshaphe the top3 with only the emotion name
+    top_3 = [x[0] for x in top_3]
+    return jsonify(top_3)
